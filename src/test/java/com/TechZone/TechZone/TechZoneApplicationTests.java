50aecfe4b33af0cdf9b907f28da6fff696c5536b
teomassicothequet <teo.massicothequet@gmail.com>
2026-01-20 10:31:24 +0100

    ajout des fichiers nécessaires pour commencer le projet, donnée par le prof

---
+++ src/test/java/com/TechZone/TechZone/TechZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechZone.TechZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
